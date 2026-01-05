5dd04c02a7744080ef620366c6425b05e09038b2
Jayen <jayen@jayenashar.org>
2026-01-06 01:09:37 +1100

    Add CloudFront rewrite function reference
    Reason: keep the live viewer-request rewrite logic tracked in git.

---
+++ infra/cloudfront/qcut-rewrite-html.js
@@ -0,0 +1,15 @@
+function handler(event) {
+  var req = event.request;
+  var uri = req.uri;
+
+  if (uri.endsWith("/")) {
+    req.uri = uri + "index.html";
+    return req;
+  }
+
+  if (!uri.includes(".")) {
+    req.uri = uri + ".html";
+  }
+
+  return req;
+}
